4b0b273babb8b50a38c554cbbc38667940c62df2
Vaibhav Gupta <vaibhavgupta@Vaibhavs-MacBook-Air.local>
2021-08-15 13:43:18 +0530

    restructuring

---
+++ WebApp/node_modules/generate-react-cli/src/templates/component/componentJsTemplate.js
@@ -0,0 +1,16 @@
+module.exports = `import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './TemplateName.module.css';
+
+const TemplateName = () => (
+  <div className={styles.TemplateName} data-testid="TemplateName">
+    TemplateName Component
+  </div>
+);
+
+TemplateName.propTypes = {};
+
+TemplateName.defaultProps = {};
+
+export default TemplateName;
+`;
---
+++ WebApp/node_modules/generate-react-cli/src/templates/component/componentTsTemplate.js
@@ -0,0 +1,11 @@
+module.exports = `import React from 'react';
+import styles from './TemplateName.module.css';
+
+const TemplateName = () => (
+  <div className={styles.TemplateName} data-testid="TemplateName">
+    TemplateName Component
+  </div>
+);
+
+export default TemplateName;
+`;
